6920464ac127c8c49b67ab4189e1eb6842be4bad
Dustin Machi <dmachi@dojotoolkit.org>
2015-06-05 23:44:20 -0400

    token validation

---
+++ validateToken.js
@@ -0,0 +1,73 @@
+var userIdRegex = /un=(\w+\@\w+(\.\w+))/
+var crypto = require("crypto");
+var request = require('request');
+var defer = require('promised-io/promise').defer;
+var when = require("promised-io/promise").when;
+
+var ss_cache = {};
+
+getSigner = function(signer){
+	var def = new defer();
+	if (ss_cache[signer]){
+		def.resolve(ss_cache[signer]);
+	}
+	request.get({url:signer,json:true}, function(err,response,body){
+		if (err) { return def.reject(err); }
+		if (!body) { return def.reject("Empty Signature"); }
+		console.log("body: ", body);
+		console.log("Signature: ", body.pubkey);
+		def.resolve(body.pubkey);
+	});
+	return def.promise;
+}
+
+var validateToken =function(token){
+	var parts = token.split("|");
+	var parsedToken = {}
+	var baseToken = []
+	parts.forEach(function(part){
+		var tuple = part.split("=");
+		if (tuple[0]!="sig"){
+			baseToken.push(part);
+		}
+		parsedToken[tuple[0]]=tuple[1];
+	});
+
+	return when(getSigner(parsedToken.SigningSubject), function(signer){
+		console.log("Got Signer Cert: ", signer);
+		console.log("Signature: ", parsedToken.sig);	
+		var verifier = crypto.createVerify("RSA-SHA1");
+		console.log("data: ", baseToken.join("|"));
+		verifier.update(baseToken.join("|"));
+		var success = verifier.verify(signer.toString("ascii"),parsedToken.sig,"hex") 
+		console.log("validation success: ", success);
+		return success;
+	}, function(err){
+		console.log("Error retrieving SigningSubject: ", parsedToken.SigningSubject);
+		return false;
+	});
+	
+}
+
+module.exports= function(token){
+	return when(validateToken(token), function(valid){
+		if (!valid) {
+			console.log("Invalid Token");
+			return false;
+		}
+
+		var user = {}
+		var matches = token.match(userIdRegex);
+
+       		if (matches && matches[1]) {
+       			user.id =  matches[1];
+		}
+
+		console.log("User from Token: ", user);
+		if (user && user.id) {
+			return user;
+		}
+		return false;
+	});
+	
+}
