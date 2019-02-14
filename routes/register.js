c5273f17b08fb859d88dd43f27993165a6bcba13
Hyunseung Yoo <hsyoo@anl.gov>
2019-02-14 13:27:07 -0600

    check illegal characters in username when register

--- routes/register.js
+++ routes/register.js
@@ -1,26 +1,28 @@
-var express = require('express')
-var router = express.Router()
-var bodyParser = require('body-parser')
-var DataModel = require('../dataModel')
-var when = require('promised-io/promise').when
-var errors = require('dactic/errors')
-var UserModel = DataModel.get('user')
-// var generateToken = require('../generateToken')
-// var validateToken = require('../validateToken')
-// var bcrypt = require('bcrypt')
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const DataModel = require('../dataModel')
+const when = require('promised-io/promise').when
+const errors = require('dactic/errors')
+const UserModel = DataModel.get('user')
 
 /* Register a new user */
 router.post('/', [
   bodyParser.urlencoded({extended: true}),
   bodyParser.json({type: 'application/json'}),
   function (req, res, next) {
+    // check for missing fields
     if (!req.body || !req.body.username || !req.body.email || !req.body.first_name || !req.body.last_name) {
       return next(new errors.BadRequest('Missing required fields'))
+    }
+    // check for user id rule
+    if (req.body.username.match(/[\w.-]/)[0] !== req.body.username) {
+      return next(new errors.BadRequest('Username contains unacceptable characters. Use letters, numbers, underscore(_), dot(.), and dash(-)'))
     }
 
     console.log('Registering New User: ', req.body.username, req.body.email)
 
-    when(UserModel.registerUser(req.body), function (results) {
+    when(UserModel.registerUser(req.body), function () {
       res.status(201)
       res.end()
     }, function (err) {
